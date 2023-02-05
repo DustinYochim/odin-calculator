27cef8abc55b497fc67903ee7a1896c2496c9587
Dustin Yochim <yochim.dustin@gmail.com>
2023-02-04 18:32:57 -0600

    Fix bug and add keyboard support

--- js/script.js
+++ js/script.js
@@ -1,113 +1,118 @@
-let displayValue = '';
-let savedNum = '';
-let operator = '';
+let displayValue = "";
+let savedNum = "";
+let operator = "";
 
-let supportedOperators = ['+', '-', 'x', '/'];
+let supportedOperators = ["+", "-", "x", "/"];
 
-const display = document.querySelector('.screen');
-const buttons = document.querySelectorAll('button');
+const display = document.querySelector(".screen");
+const buttons = document.querySelectorAll("button");
 
 function add(x, y) {
-    return x + y;
+  return x + y;
 }
 
 function subtract(x, y) {
-    return x - y;
+  return x - y;
 }
 
-function multiply(x,y) {
-    return x * y;
+function multiply(x, y) {
+  return x * y;
 }
 
-function divide(x,y) {
-    if (y === 0) {
-        alert("Cannot divide by zero");
-        return;
-    }
-    return x / y;
+function divide(x, y) {
+  if (y === 0) {
+    alert("Cannot divide by zero");
+    return;
+  }
+  return x / y;
 }
 
 function operate(operator, x, y) {
-    switch(operator) {
-        case '+':
-            return Math.round((add(x,y) + Number.EPSILON) * 100) / 100;
-            break;
-        case '-':
-            return Math.round((subtract(x,y) + Number.EPSILON) * 100) / 100;
-            break;
-        case 'x':
-            return Math.round((multiply(x,y) + Number.EPSILON) * 100) / 100;
-            break;
-        case '/':
-            return Math.round((divide(x,y) + Number.EPSILON) * 100) / 100;
-            break;
-    }
+  switch (operator) {
+    case "+":
+      return Math.round((add(x, y) + Number.EPSILON) * 100) / 100;
+      break;
+    case "-":
+      return Math.round((subtract(x, y) + Number.EPSILON) * 100) / 100;
+      break;
+    case "x":
+      return Math.round((multiply(x, y) + Number.EPSILON) * 100) / 100;
+      break;
+    case "/":
+      return Math.round((divide(x, y) + Number.EPSILON) * 100) / 100;
+      break;
+  }
 }
 
 function populateDisplay() {
-    display.textContent = displayValue;
+  display.textContent = displayValue;
 }
 
 function clearClicked() {
-    displayValue = '';
-    savedNum = '';
-    operator = '';
-    populateDisplay();
+  displayValue = "";
+  savedNum = "";
+  operator = "";
+  populateDisplay();
 }
 
 function deleteClicked() {
-    if (displayValue.length >= 1) {
-        displayValue = displayValue.slice(0, displayValue.length - 1);
-        populateDisplay();
-    }
+  if (displayValue.length >= 1) {
+    displayValue = displayValue.slice(0, displayValue.length - 1);
+    populateDisplay();
+  }
 }
 
 function numberClicked(id) {
-    if (id === '.' && displayValue.includes('.')) return;
-    displayValue += id;
-    populateDisplay();
+  if (id === "." && displayValue.includes(".")) return;
+  displayValue += id;
+  populateDisplay();
 }
 
 function operatorClicked(id) {
-    if (operator !== '') {
-        displayValue = operate(operator, +savedNum, +displayValue);
-        populateDisplay();
-        savedNum = displayValue;
-        displayValue = '';
-        operator = id;
-    } else {
-        savedNum = displayValue;
-        displayValue = '';
-        operator = id;
-    }
+  if (operator !== "") {
+    displayValue = operate(operator, +savedNum, +displayValue);
+    populateDisplay();
+    savedNum = displayValue;
+    displayValue = "";
+    operator = id;
+  } else {
+    savedNum = displayValue;
+    displayValue = "";
+    operator = id;
+  }
 }
 
 function equalsClicked() {
-    if (displayValue !== '' && savedNum !== '' && operator !== '') {
-        displayValue = operate(operator, +savedNum, +displayValue);
-        populateDisplay();
-        savedNum = displayValue;
-        displayValue = '';
-        operator = '';
-    }
+  if (displayValue !== "" && savedNum !== "" && operator !== "") {
+    displayValue = operate(operator, +savedNum, +displayValue);
+    populateDisplay();
+    savedNum = displayValue;
+    operator = "";
+  }
 }
 
 function handleButtonClick(id) {
-    if (id === 'clear') {
-        clearClicked();
-    } else if (id === 'delete') {
-        deleteClicked();
-    } else if (supportedOperators.includes(id)) {
-        operatorClicked(id);
-    } else if (id === '=') {
-        equalsClicked();
-    } else if (!isNaN(id) || id === '.') {
-        numberClicked(id);
-    }
+  if (id === "clear") {
+    clearClicked();
+  } else if (id === "delete") {
+    deleteClicked();
+  } else if (supportedOperators.includes(id)) {
+    operatorClicked(id);
+  } else if (id === "=") {
+    equalsClicked();
+  } else if (!isNaN(id) || id === ".") {
+    numberClicked(id);
+  }
 }
 
 buttons.forEach((button) => {
-    button.addEventListener('click', () => {
-        handleButtonClick(button.id);
-    });
+  button.addEventListener("click", () => {
+    handleButtonClick(button.id);
+  });
+});
+
+window.addEventListener("keydown", function (e) {
+  const key = document.querySelector(`button[data-key="${e.key}"]`);
+  if (!key) return;
+  handleButtonClick(key.id);
 });
